b024535be6e5671c120ce2e975fe8bd68b58026c
Eric Ma <ericmajinglong@gmail.com>
2016-01-12 17:18:00 -0500

    added exposure mode = beach

--- picam.py
+++ picam.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
 
     while True:
         with pc.PiCamera(resolution=(2560,1920)) as p:
+            p.exposure_mode = 'beach'
             now = dt.now()
             p.capture('{0}/{1}.jpg'.format(imgdir, str(now)), format='jpeg')
             sleep(60)
